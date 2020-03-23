147dde8e9a0c9db4ad0964297a0302bcad329b11
Junko Tahara <junkotahara911@gmail.com>
2020-03-23 17:42:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    array.detect
+    array.detect {|item| 
   else
     nil
   end
