5c16ba1b6b4796f6dae9ed9afb016b4497b411fc
Junko Tahara <junkotahara911@gmail.com>
2020-03-23 17:32:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,4 +21,10 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include
+  
+  
+  
 end
+
+
