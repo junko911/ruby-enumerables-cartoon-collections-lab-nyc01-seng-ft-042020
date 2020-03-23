9f47ea5726587b3574ea18e6f913f858c01a9196
Junko Tahara <junkotahara911@gmail.com>
2020-03-23 17:30:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |item|
+    item.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|word| word.length >4}
 end
 
 def find_the_cheese# code an argument here
